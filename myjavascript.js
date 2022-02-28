b602d3613f6f685b0f58a19857b654e5d3e319b5
edodima13 <edodima13@libero.it>
2022-02-28 18:08:05 +0100

    avanzamento

--- myjavascript.js
+++ myjavascript.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: "#root",
     data: {
+        newItems :"",
         todoItems : [
             {
                 text : "fare la spesa",
@@ -15,11 +16,21 @@ var app = new Vue({
                 done : false
             }
         ],
+        
     } ,
     methods : {
+    addItems : function(){
+        const newItems = {
+            text:this.newItems,
+            done:false
+        }
+        console.log(newItems)
+        this.todoItems.push(this.newItems)
+    },
     removeToDo : function(index) {
         this.todoItems.splice(index, 1)
     }
+    
     }
 })
 
